a40724e59d984fc6a85c6ceb0d13d1cef6eba6ca
a3mqq2 <aishaaltery03@gmail.com>
2026-02-12 22:05:03 +0200

    فثسف

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,12 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+Artisan::command('cron:test', function () {
+    $log = storage_path('logs/cron.log');
+    $time = now()->format('Y-m-d H:i:s');
+    file_put_contents($log, "[{$time}] Cron is working!\n", FILE_APPEND);
+    $this->info('Cron test logged at ' . $time);
+})->purpose('Test cron job');
+
 Schedule::command('db:backup')->dailyAt('23:00');
+Schedule::command('cron:test')->everyMinute();
